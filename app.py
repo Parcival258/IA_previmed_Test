a8142e4a260b0f4191205f8656ce5ea2f5acd001
parvical258 <andreslasso250@gmail.com>
2025-11-05 06:42:09 -0500

    deploy 4

--- app.py
+++ app.py
@@ -274,3 +274,8 @@ async def responder(mensaje: MensajeEntrada):
 @app.get("/")
 def inicio():
     return {"mensaje": "🤖 Asistente IA Previmed operativo"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
